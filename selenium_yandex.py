227de18450f9c15011dd3aabd944e08b8aaddd87
typeoflife <lovesun-83@mail.ru>
2022-07-29 09:02:17 +1000

    2 tests added

--- selenium_yandex.py
+++ selenium_yandex.py
@@ -1,9 +1,11 @@
 import unittest
 from selenium import webdriver
+from selenium.common import NoSuchElementException
 from selenium.webdriver import Keys
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 import time
+import random
 
 
 class ChromeTest(unittest.TestCase):
@@ -17,26 +19,26 @@ class ChromeTest(unittest.TestCase):
         driver.get('https://passport.yandex.ru/auth/')
         elem = driver.find_element(By.XPATH, "//input[@CLASS='Textinput-Control']")
         time.sleep(1)
-        elem.send_keys('Hoffa368')
+        elem.send_keys('my_login')
         time.sleep(1)
         elem.send_keys(Keys.RETURN)
         time.sleep(1)
         elem = driver.find_element(By.XPATH, "//input[@CLASS='Textinput-Control']")
         time.sleep(1)
-        elem.send_keys('imbaimba!23')
+        elem.send_keys('my_password')
         elem.send_keys(Keys.RETURN)
         time.sleep(5)
         name_acc = driver.find_element(
             By.XPATH, "//div[@CLASS='personal-info-login__text personal-info-login__text_decorated']").text
 
-        assert name_acc == "Hoffa368"
+        assert name_acc == "my_account_name"
 
     def test_failure_login(self):
         driver = self.driver
         driver.get('https://passport.yandex.ru/auth/')
         elem = driver.find_element(By.XPATH, "//input[@CLASS='Textinput-Control']")
         time.sleep(1)
-        elem.send_keys('Hoffa368')
+        elem.send_keys('my_login')
         time.sleep(1)
         elem.send_keys(Keys.RETURN)
         time.sleep(1)
@@ -48,9 +50,39 @@ class ChromeTest(unittest.TestCase):
         text_message = driver.find_element(
             By.XPATH, "//div[@CLASS='Textinput-Hint Textinput-Hint_state_error']").text
 
-        print(text_message)
-
         assert text_message == "Неверный пароль"
+
+    def test_yandex_follow(self):
+        driver = self.driver
+        driver.get('https://passport.yandex.ru/auth/')
+        driver.find_element(By.XPATH, "//a[@CLASS='Logo Logo_ya']").click()
+        time.sleep(2)
+        try:
+            finder = driver.find_element(
+                By.XPATH, "//input[@CLASS='input__control input__input mini-suggest__input']")
+        except NoSuchElementException:
+            return False
+
+        assert finder
+
+    def test_yandex_create_button(self):
+        driver = self.driver
+        driver.get('https://passport.yandex.ru/auth/')
+        time.sleep(1)
+        try:
+            driver.find_element(
+                By.XPATH, "//button[@CLASS='Button2 Button2_size_l Button2_view_clear']").click()
+            imput_tel = driver.find_element(By.XPATH, "//input[@CLASS='Textinput-Control']")
+            imput_tel.send_keys(f'+5{random.randrange(1111111111, 9999999999, 10)}')
+            imput_tel.send_keys(Keys.RETURN)
+            text_message = driver.find_element(
+                By.XPATH, "//div[@CLASS='Textinput-Hint Textinput-Hint_state_error']").text
+
+
+        except NoSuchElementException:
+            return False
+
+        assert text_message
 
 
 if __name__ == '__main__':
